2e7cdfb1e01c456bf78eaa0337988ec72372dd45
Shubbu-07 <95517382+Shubbu-07@users.noreply.github.com>
2024-05-16 10:49:57 +0530

    Add project files.

---
+++ ECommerceApp/AppData/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using ECommerceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApp.AppData
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.Total)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<OrderItem>()
+                .Property(oi => oi.UnitPrice)
+                .HasPrecision(18, 2);
+        }
+    }
+}
---
+++ ECommerceApp/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceApp.Models
+{
+    public class Order
+    {
+        public int? Id { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public decimal? Total { get; set; }
+        public string? Status { get; set; }
+        public ICollection<OrderItem>? OrderItems { get; set; }
+    }
+}
