0250eadb68bad17c79948ff9cb572c053bc0bb7f
Shubbu-07 <95517382+Shubbu-07@users.noreply.github.com>
2024-05-18 21:14:30 +0530

    Updated Navbar and Products controller view.

---
+++ ECommerceApp/Controllers/ProductsController.cs
@@ -0,0 +1,85 @@
+﻿using ECommerceApp.AppData;
+using ECommerceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Products
+        [HttpGet]
+        public IActionResult Display()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+        // GET: Products/Details/5
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var product = _context.Products.Find(id);
+            return View(product);
+        }
+        // GET: Products/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Display));
+            }
+            return View(product);
+        }
+        // GET: Products/Edit/5
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _context.Products.Find(id);
+            return View(product);
+        }
+        // POST: Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Update(product);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Display));
+            }
+            return View(product);
+        }
+        // GET: Products/Delete/5
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            return View(product);
+        }
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = _context.Products.Find(id);
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Display));
+        }
+    }
+}
